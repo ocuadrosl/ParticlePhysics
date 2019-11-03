e69e56d28fd460a69d834ca8ec649aa8a98f8249
Oscar <ocuadrosl@gmail.com>
2019-11-03 16:22:28 -0300

    triangle

--- main.cpp
+++ main.cpp
@@ -1,50 +1,133 @@
 #include <iostream>
+#include <memory>
 
 #include <GL/glew.h>
-#include <GL/glut.h>
+#include <GLFW/glfw3.h>
+//#include <GL/glut.h>
+
+
+
 
 #include "particlesystem.h"
 
-using namespace std;
 
 
-void displayMe(void)
-{
-    /*
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.5, 0.5, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-    glEnd();
-    glFlush();
-    */
-
-    ParticleSystem particleSystem;
-    particleSystem.setNumberOfParticles(100);
-    particleSystem.updateSystem();
-
-    glVertexPointer( 3, GL_FLOAT, sizeof(Particle), nullptr);
-    glDrawElements(GL_TRIANGLES, 1, GL_UNSIGNED_INT, nullptr);
-
-
-}
-
-
-
-
-int main(int argc, char** argv)
+int main()
 {
 
+    //GLFWwindow* window;
 
-    glutInit(&argc, argv);
-    glEnable(GL_BLEND);
-    glutInitDisplayMode(GLUT_RGBA);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Particle Physics");
-    glutDisplayFunc(displayMe);
-    glutMainLoop();
+    // Initialise GLFW
+    glewExperimental = true; // Needed for core profile
+    if( !glfwInit() )
+    {
+        std::cerr<<"Failed to initialize GLFW\n"<<std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS needs it
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Open a window and create its OpenGL context
+
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Partices System", nullptr,nullptr);
+
+    if( window == nullptr ){
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    // Dark blue background
+    glClearColor(0.2f, 0.2f, 0.5f, 0.0f);
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+
+    std::vector<float> points = {
+       0.0f,  0.5f,  0.0f,
+       0.5f, -0.5f,  0.0f,
+      -0.5f, -0.5f,  0.0f
+    };
+
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), &points[0], GL_STATIC_DRAW);
+
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    //shaders
+    const char* vertex_shader =
+            "#version 400\n"
+            "in vec3 vp;"
+            "void main() {"
+            "  gl_Position = vec4(vp, 1.0);"
+            "}";
+
+    const char* fragment_shader =
+            "#version 400\n"
+            "out vec4 frag_colour;"
+            "void main() {"
+            "  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+            "}";
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shader_programme = glCreateProgram();
+    glAttachShader(shader_programme, fs);
+    glAttachShader(shader_programme, vs);
+    glLinkProgram(shader_programme);
+
+    do{
+        // Clear the screen.
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shader_programme);
+        glBindVertexArray(vao);
+        // draw points 0-3 from the currently bound VAO with current in-use shader
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // update other events like input handling
+        glfwPollEvents();
+        // put the stuff we've been drawing onto the display
+        glfwSwapBuffers(window);
+
+
+    } // Check if the ESC key was pressed or the window was closed
+    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&  glfwWindowShouldClose(window) == 0 );
+
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
+
     return 0;
 }
